7986d3d040a23b69562500cc1e2a150b969a026f
Greattie <gamubode@gmail.com>
2023-03-28 14:23:58 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const hamburger = document.querySelector('.hamburger-menu')
+console.log(hamburger)
+
+const btn = document.getElementById('btn')
+
+btn.addEventListener('click', function(){
+    alert('shes good')
+})
